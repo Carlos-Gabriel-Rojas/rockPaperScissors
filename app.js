ee774d7e3e12a95e3d956266f54d30b290b3e625
Carlos-Gabriel-Rojas <carlosgabrielrojas.dev@gmail.com>
2023-02-26 11:25:08 -0300

    computerChoice function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // computer choice function
 
-
+function computerPlay() {
+    let choice = ["rock", "paper", "scissors"];
+    return choice[Math.floor(math.random() * 3)];
+}
 
 //player choice function
 
